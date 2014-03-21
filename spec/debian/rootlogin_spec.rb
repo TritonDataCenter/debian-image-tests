747537fbbc525d0f06e6079d06ec06a1b3c601df
Christopher Horrell <christopher@horrell.ca>
2014-03-21 14:30:10 -0400

    Better checking for root password

--- spec/debian/rootlogin_spec.rb
+++ spec/debian/rootlogin_spec.rb
@@ -2,9 +2,16 @@ require 'spec_helper'
 
 # Ensure root password is set and password matches value in metadata
 # Need to somehow validate that it's not the same as livecd and that
-# it matche value from mdata-get
+# it matches value from mdata-get
 
-# 
-#describe command('cat /etc/passwd | grep root | awk -F':' '{print $2;}'') do
-#  it { should_not return_stdout "x" }
-#end
+# Make sure default live-build password is not set
+# We want to explicitly test for this first to ensure the default password from
+# the live-build is not leaking through.
+describe command('cat /etc/shadow | grep root | awk -F':' '{print $2;}'') do
+  it { should_not return_stdout "$1$qHFOhAjw$AjgoyfS8u32uqV8qsL1r70" }
+end
+
+# Make sure no password is set
+describe command('cat /etc/shadow | grep root | awk -F':' '{print $2;}'') do
+  it { should return_stdout "" }
+end

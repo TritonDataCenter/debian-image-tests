07b258563dc2a05b86f3d99ac214b205b21fd123
Christopher Horrell <christopher@joyent.com>
2014-03-24 14:25:30 +0000

    Test to ensure the hostname is set to the uuid of the VM

--- spec/debian/hostname_spec.rb
+++ spec/debian/hostname_spec.rb
@@ -4,3 +4,9 @@ require 'spec_helper'
 describe command('hostname') do
   it { should_not return_stdout 'debian-7-blank' }
 end
+
+# Hostname should retunr the UUID
+uuid = `mdata-get sdc:uuid`
+describe command('hostname') do
+  it { should return_stdout uuid }
+end

03d0de4d4108d6a82819b80e015e57a99d09ca52
Christopher Horrell <christopher@joyent.com>
2014-03-25 14:33:06 +0000

    Test to ensure PasswordAuthentication is set to no. IMAGE-459

--- spec/debian/rootlogin_spec.rb
+++ spec/debian/rootlogin_spec.rb
@@ -15,3 +15,10 @@ end
 describe command("cat /etc/shadow | grep root | awk -F':' '{print $2;}'") do
   it { should return_stdout "" }
 end
+
+# Make sure ssh login is via ssh key only. This is required since we are enabling
+# a password for root per IMAGE-459
+describe file('/etc/ssh/sshd_config') do
+  it { should be_file }
+  it { should contain "PasswordAuthentication no" }
+end

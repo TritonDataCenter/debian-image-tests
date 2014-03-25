a4749c6edb0d7723c2eb48595f39d530816aea94
Christopher Horrell <christopher@joyent.com>
2014-03-25 01:19:45 +0000

    Fixed tests to ensure hostname from live-build is not set

--- spec/debian/hostname_spec.rb
+++ spec/debian/hostname_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 # Ensure hostname is not carried over from live-cd build
 describe command('hostname') do
-  it { should_not return_stdout 'debian-7-blank' }
+  it { should_not return_stdout 'debian-7' }
 end
 
-# Hostname should retunr the UUID
-uuid = `mdata-get sdc:uuid`
-describe command('hostname') do
-  it { should return_stdout uuid }
-end

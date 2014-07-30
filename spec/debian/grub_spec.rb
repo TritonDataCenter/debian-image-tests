bd8844651719a1e9acf47e7dfe7b41360bdb6ffb
Christopher Horrell <christopher@joyent.com>
2014-07-30 18:29:47 +0000

    Test to ensure 5 second grub timout. IMAGE-543

--- spec/debian/grub_spec.rb
+++ spec/debian/grub_spec.rb
@@ -4,4 +4,5 @@ describe file('/etc/default/grub') do
 	it { should be_file }
 	it { should contain "tsc=reliable" }
   it { should contain "divider=10" }
+  it { should contain "GRUB_TIMEOUT=5" }
 end

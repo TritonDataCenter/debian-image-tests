98b64d24a4d5d4268ddbdbc5906773f3a99bcf80
Christopher Horrell <christopher@joyent.com>
2015-05-27 16:49:06 -0400

    Exclude testing for certain packages on Debian 8
    We're slimming down the list of pre-installed packages starting with
    jessie

--- spec/debian/corepackages_spec.rb
+++ spec/debian/corepackages_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
-describe package('acpid') do
-  it { should be_installed }
+# Packages only installed on the Debian 7 (wheezy) image
+if property[:name] =~ /(wheezy)/
+  describe package('bzip2') do
+    it { should be_installed }
+  end
+  
+  describe package('htop') do
+    it { should be_installed }
+  end
+  
+  describe package('mlocate') do
+    it { should be_installed }
+  end
+  
+  describe package('python') do
+    it { should be_installed }
+  end
 end
 
-describe package('bzip2') do
+describe package('acpid') do
   it { should be_installed }
 end
 
@@ -12,15 +27,7 @@ describe package('curl') do
   it { should be_installed }
 end
 
-describe package('htop') do
-  it { should be_installed }
-end
-
 describe package('less') do
-  it { should be_installed }
-end
-
-describe package('mlocate') do
   it { should be_installed }
 end
 
@@ -36,10 +43,6 @@ describe package('parted') do
   it { should be_installed }
 end
 
-describe package('python') do
-  it { should be_installed }
-end
-
 describe package('vim') do
   it { should be_installed }
 end
@@ -47,7 +50,5 @@ end
 describe package('wget') do
   it { should be_installed }
 end
-
-
 
 

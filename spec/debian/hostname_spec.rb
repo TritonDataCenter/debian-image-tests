d4193d4bc641ed474679c6f681d2602d0c500b30
Christopher Horrell <christopher@joyent.com>
2015-06-30 15:19:36 -0400

    Make sure hostname isn't being carried over from build machine

--- spec/debian/hostname_spec.rb
+++ spec/debian/hostname_spec.rb
@@ -5,6 +5,11 @@ describe command('hostname') do
   its(:stdout) { should_not eq 'debian-7' }
 end
 
+# Ensure hostname is not carried over from live-cd build
+describe command('hostname') do
+  its(:stdout) { should_not eq 'debian-8' }
+end
+
 describe command('hostname') do
   its(:stdout) { should_not eq 'bad' }
 end

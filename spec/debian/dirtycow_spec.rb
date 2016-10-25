6e8c364c721c54482acdbd4ec9b7a4782286ca31
Christopher Horrell <christopher@joyent.com>
2016-10-25 13:16:36 -0400

    Add dirtycow test

---
+++ spec/debian/dirtycow_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+# Kernel version should at least be 3.2.82-1
+# The vulnerable version is 3.2.78-1
+# See: https://security-tracker.debian.org/tracker/CVE-2016-5195
+# In time this test will fail as the kernel is updated, but it's useful for now
+# to ensure this is patched.
+
+describe command('dpkg-query -l | grep linux-image | grep -q 3.2.82-1') do
+  its(:exit_status) { should eq 0 }
+end

5cd0ae6e5d048e9600fd20cd172d545ac62944b0
Christopher Horrell <christopher@joyent.com>
2017-04-10 16:35:12 -0400

    Drop dirty cow test for Debian 8
    This is old enough that we can drop it now

--- spec/debian/dirtycow_spec.rb
+++ spec/debian/dirtycow_spec.rb
@@ -11,17 +11,3 @@ if command('. /etc/os-release && echo $VERSION_ID | grep -q 7').exit_status == 0
     its(:exit_status) { should eq 0 }
   end
 end
-
-
-
-# Kernel version should at least be 3.16.36-1+deb8u2
-# The vulnerable version is 3.16.36-1+deb8u1
-# See: https://security-tracker.debian.org/tracker/CVE-2016-5195
-# In time this test will fail as the kernel is updated, but it's useful for now
-# to ensure this is patched.
-
-if command('. /etc/os-release && echo $VERSION_ID | grep -q 8').exit_status == 0
-  describe command('dpkg-query -l | grep linux-image | grep -q 3.16.36-1+deb8u2') do
-    its(:exit_status) { should eq 0 }
-  end
-end
